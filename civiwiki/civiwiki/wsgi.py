652b53252381c2f59797204d8816df0d32b7b9c8
Darius Calliet <dcalliet@topoppssoftware.com>
2016-06-05 16:34:44 -0500

    updating the wsgi file

--- civiwiki/civiwiki/wsgi.py
+++ civiwiki/civiwiki/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "civiwiki.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "civiwiki.settings.production")
 
 application = get_wsgi_application()
