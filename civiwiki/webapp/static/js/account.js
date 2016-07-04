92fa378355a8ff262c28cada3d840819190331f3
Daniel Borstelmann <dborstelmann@wustl.edu>
2016-07-04 17:47:38 -0500

    saving accounts

--- civiwiki/webapp/static/js/account.js
+++ civiwiki/webapp/static/js/account.js
@@ -14,6 +14,30 @@ cw.AccountView = BB.View.extend({
     },
 
     events: {
+        'blur .save-account': 'saveAccount',
+        'keypress .save-account': 'calcKey'
+    },
+
+    calcKey: function (e) {
+        if (e.which == 13 && !e.shiftKey) {
+            e.preventDefault();
+            $(e.target).blur();
+        }
+    },
+
+    saveAccount: function (e) {
+        var $this = $(e.target),
+            changeKey = $this.attr('id'),
+            changeVal = $this.val(),
+            apiData = {};
+
+        apiData[changeKey] = changeVal;
+
+        $.ajax({
+            url: 'api/edituser',
+            type: 'POST',
+            data: apiData
+        });
     },
 
 });
