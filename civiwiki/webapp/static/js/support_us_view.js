fa9fde60c90edc59f3e4bec7e8661ec58e4acf9c
jl24 <jooheelee@wustl.edu>
2016-07-10 20:01:06 -0500

    removed code for bugs that the new materialize fixed

--- civiwiki/webapp/static/js/support_us_view.js
+++ civiwiki/webapp/static/js/support_us_view.js
@@ -11,16 +11,12 @@ cw.SupportusView = BB.View.extend({
     render: function () {
         this.$el.empty().append(this.supportusTemplate());
         this.setupStaticNav();
-        // Following initializes parallax, an effect where the background moves slower than the foreground
         $('.parallax').parallax();
-
-        // Code to configure twitter sharing from the twitter website
-        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
     },
 
+    // Setup sidebar functionality
     setupStaticNav: function () {
         $('.button-collapse').sideNav();
         $('.collapsible').collapsible();
-        $('.sideNav').css('display', 'inherit');
     }
 });
