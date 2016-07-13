1d123b554e9553bc4aa49195b25d3e469a45c7ab
Daniel Borstelmann <dborstelmann@wustl.edu>
2016-07-13 00:56:30 -0500

    GAH

--- civiwiki/settings.py
+++ civiwiki/settings.py
@@ -108,7 +108,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../static'),
+    os.path.join(BASE_DIR, '../webapp/static'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
