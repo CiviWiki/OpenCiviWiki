075d921e376df6ad6c442ad82060cc4a0face789
Daniel Borstelmann <dborstelmann@wustl.edu>
2016-07-13 00:09:14 -0500

    fix

--- civiwiki/settings.py
+++ civiwiki/settings.py
@@ -19,7 +19,7 @@ def get_env_variable(environment_variable, optional=False):
             error = "environment variable '{ev}' not found.".format(ev=environment_variable)
             raise ImproperlyConfigured(error)
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = get_env_variable("DJANGO_SECRET_KEY")
 SUNLIGHT_API_KEY = get_env_variable("SUNLIGHT_API_KEY")
 ALLOWED_HOSTS = [".herokuapp.com", ".civiwiki.org"]
