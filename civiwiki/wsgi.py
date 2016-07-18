70e68f7e0ae1a8d5fd4c1df003943230e3ec57e6
jl24 <jooheelee@wustl.edu>
2016-07-18 10:01:15 -0500

    beta html adjust

--- civiwiki/wsgi.py
+++ civiwiki/wsgi.py
@@ -12,7 +12,7 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "civiwiki.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+#from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+#application = DjangoWhiteNoise(application)
