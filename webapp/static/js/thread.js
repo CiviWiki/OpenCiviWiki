6f0c0fc59129c7a102e8b47f98490e4e39b19124
Daniel Borstelmann <dborstelmann@wustl.edu>
2016-08-20 15:53:54 -0500

    well, that's a lot of threads done

--- webapp/static/js/thread.js
+++ webapp/static/js/thread.js
@@ -23,7 +23,7 @@ cw.ThreadView = BB.View.extend({
         this.username = options.username;
 
         this.listenTo(this.model, 'sync', function () {
-            this.wikiRender();
+            this.threadWikiRender();
             this.threadBodyRender();
         });
 
@@ -34,7 +34,7 @@ cw.ThreadView = BB.View.extend({
         this.$el.empty().append(this.template());
     },
 
-    wikiRender: function () {
+    threadWikiRender: function () {
         if (this.$('.thread-wiki-holder').length) {
             this.$('.thread-wiki-holder').empty().append(this.wikiTemplate());
         }
@@ -43,10 +43,50 @@ cw.ThreadView = BB.View.extend({
     threadBodyRender: function () {
         if (this.$('.thread-body-holder').length) {
             this.$('.thread-body-holder').empty().append(this.bodyTemplate());
+
+            var $civiNavScroll = this.$('.civi-outline');
+            $civiNavScroll.css({height: $('body').height() - $civiNavScroll.offset().top + 8});
+            var $civiThreadScroll = this.$('.main-thread');
+            $civiThreadScroll.css({height: $('body').height() - $civiThreadScroll.offset().top - 72});
         }
     },
 
     events: {
+        'click .enter-body': 'scrollToBody',
+        'click .enter-wiki': 'scrollToWiki',
+        'click .expand-nav': 'expandNav'
+    },
+
+    scrollToBody: function () {
+        this.$('.thread-body-holder').css({display: 'block'});
+        $('body').css({overflow: 'hidden'});
+
+        $('body').animate({
+            scrollTop: $('.thread-body-holder').offset().top
+        }, 1000);
+    },
+
+    scrollToWiki: function () {
+        var _this = this;
+
+        $('body').animate({
+            scrollTop: 0
+        }, 1000, function () {
+            _this.$('.thread-body-holder').css({display: 'none'});
+        });
+        $('body').css({overflow: 'scroll'});
+    },
+
+    expandNav: function (e) {
+        var $this = $(e.target);
+
+        if ($this.hasClass('expanded')) {
+            $('.civi-nav-wrapper').slideUp();
+            $this.removeClass('expanded');
+        } else {
+            $('.civi-nav-wrapper').slideDown();
+            $this.addClass('expanded');
+        }
     }
 
 });
