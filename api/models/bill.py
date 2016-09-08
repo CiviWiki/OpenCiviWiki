1b32b81e80a28940ba30aa1adda27a7bdb1f41e5
Daniel Borstelmann <dborstelmann@wustl.edu>
2016-09-07 23:28:12 -0500

    remove circular import

--- api/models/bill.py
+++ api/models/bill.py
@@ -1,10 +1,8 @@
 from django.db import models
-from civi import Civi
 
 
 class Bill(models.Model):
     id = models.CharField(max_length=255, primary_key=True) # from sunlight
-    civi = models.ForeignKey(Civi, default=None, null=True) # always a solution or null
 
     title = models.CharField(max_length=1023)
     short_title = models.CharField(max_length=1023)
