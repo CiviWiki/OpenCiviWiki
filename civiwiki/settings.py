9cc7e0fd862a9ed53716d0809b08eebfbc7a381f
Daniel Borstelmann <dborstelmann@wustl.edu>
2016-12-08 13:25:37 -0600

    updated models

--- civiwiki/settings.py
+++ civiwiki/settings.py
@@ -81,6 +81,7 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if 'CIVIWIKI_LOCAL_NAME' not in os.environ:
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
