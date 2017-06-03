60ed2051d85fcb07e82baec015f3f4341f8853a4
jl24 <jooheelee@wustl.edu>
2017-06-03 13:54:57 -0500

    migration: Remove link references

--- api/migrations/0006_auto_20170110_0519.py
+++ api/migrations/0006_auto_20170110_0519.py
@@ -16,15 +16,15 @@ class Migration(migrations.Migration):
             model_name='civi',
             name='related_civis',
         ),
-        migrations.AddField(
-            model_name='civi',
-            name='links',
-            field=models.ManyToManyField(related_name='_civi_links_+', to='api.Civi'),
-        ),
-        migrations.RemoveField(
-            model_name='civi',
-            name='links',
-        ),
+        # migrations.AddField(
+        #     model_name='civi',
+        #     name='links',
+        #     field=models.ManyToManyField(related_name='_civi_links_+', to='api.Civi'),
+        # ),
+        # migrations.RemoveField(
+        #     model_name='civi',
+        #     name='links',
+        # ),
 
         migrations.AddField(
             model_name='civi',
