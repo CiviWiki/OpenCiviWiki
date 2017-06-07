1f5bbdf76d61606adb55048b67e75c41789d262c
jl24 <jooheelee@wustl.edu>
2017-06-07 06:10:07 -0500

    Migrations change for dev db

--- api/migrations/0020_auto_20170418_1652.py
+++ api/migrations/0020_auto_20170418_1652.py
@@ -2,23 +2,25 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-
+from django.conf import settings
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('api', '0019_auto_20170418_0727'),
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='thread',
-            name='level',
-            field=models.CharField(default=b'federal', max_length=31, choices=[(b'federal', b'Federal'), (b'state', b'State')]),
-        ),
-        migrations.AddField(
-            model_name='thread',
-            name='state',
-            field=models.CharField(blank=True, max_length=2, choices=[(b'AL', b'Alabama'), (b'AK', b'Alaska'), (b'AZ', b'Arizona'), (b'AR', b'Arkansas'), (b'CA', b'California'), (b'CO', b'Colorado'), (b'CT', b'Connecticut'), (b'DE', b'Delaware'), (b'DC', b'District of Columbia'), (b'FL', b'Florida'), (b'GA', b'Georgia'), (b'HI', b'Hawaii'), (b'ID', b'Idaho'), (b'IL', b'Illinois'), (b'IN', b'Indiana'), (b'IA', b'Iowa'), (b'KS', b'Kansas'), (b'KY', b'Kentucky'), (b'LA', b'Louisiana'), (b'ME', b'Maine'), (b'MD', b'Maryland'), (b'MA', b'Massachusetts'), (b'MI', b'Michigan'), (b'MN', b'Minnesota'), (b'MS', b'Mississippi'), (b'MO', b'Missouri'), (b'MT', b'Montana'), (b'NE', b'Nebraska'), (b'NV', b'Nevada'), (b'NH', b'New Hampshire'), (b'NJ', b'New Jersey'), (b'NM', b'New Mexico'), (b'NY', b'New York'), (b'NC', b'North Carolina'), (b'ND', b'North Dakota'), (b'OH', b'Ohio'), (b'OK', b'Oklahoma'), (b'OR', b'Oregon'), (b'PA', b'Pennsylvania'), (b'RI', b'Rhode Island'), (b'SC', b'South Carolina'), (b'SD', b'South Dakota'), (b'TN', b'Tennessee'), (b'TX', b'Texas'), (b'UT', b'Utah'), (b'VT', b'Vermont'), (b'VA', b'Virginia'), (b'WA', b'Washington'), (b'WV', b'West Virginia'), (b'WI', b'Wisconsin'), (b'WY', b'Wyoming')]),
+        migrations.CreateModel(
+            name='Invitation',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('invitee_email', models.EmailField(default=None, max_length=254)),
+                ('verification_code', models.CharField(max_length=31)),
+                ('date_created', models.DateTimeField(auto_now_add=True, null=True)),
+                ('host_user', models.ForeignKey(related_name='hosts', default=None, to=settings.AUTH_USER_MODEL, null=True)),
+                ('invitee_user', models.ForeignKey(related_name='invitees', default=None, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
         ),
     ]
