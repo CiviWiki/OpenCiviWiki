ed65cd712bb44aef3e5a643d9ddbdf957188213e
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-08-10 18:01:27 +0300

    Get data from dummy API

--- api/v1/views.py
+++ api/v1/views.py
@@ -1,8 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import requests
 
 @api_view(['GET'])
 def test_endpoint(request):
     if request.method == 'GET':
-
-        return Response('test response')
+        # Create a request to fake data API
+        api_response = requests.get("https://reqres.in/api/users/2")
+        
+        return Response(api_response)
