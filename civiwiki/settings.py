569b6024d3e16683bc192c91db659b0dddf1d20c
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-08-10 17:39:24 +0300

    Add rest_framework app

--- civiwiki/settings.py
+++ civiwiki/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'storages',
     'channels',
     'civiwiki',
