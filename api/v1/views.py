e483d33b1c7bb48a2ec696dcf698acfa72dd04c7
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-08-11 17:20:00 +0300

    Initial working Open States API endpoint

--- api/v1/views.py
+++ api/v1/views.py
@@ -1,13 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import requests
+import pyopenstates as open_states
 
 from django.conf import settings
 
 @api_view(['GET'])
 def open_states_api_endpoint(request):
     if request.method == 'GET':
-        # Create a request to fake data API
-        api_response = requests.get("https://reqres.in/api/users/2")
+        # Get OpenStates API key
+        open_states_api_key = settings.OPEN_STATES_API_KEY
+
+        # Initialize OpenStates with API key
+        open_states.set_api_key(open_states_api_key)
+
+        # Get some data from OpenStates API
+        api_response = open_states.search_bills(q='methane')
 
         return Response(api_response)
