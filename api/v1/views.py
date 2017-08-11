f0cbdc762c3ab5c60773a7527281bccc65cf6d85
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-08-11 17:02:34 +0300

    Import settings

--- api/v1/views.py
+++ api/v1/views.py
@@ -2,10 +2,12 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import requests
 
+from django.conf import settings
+
 @api_view(['GET'])
 def test_endpoint(request):
     if request.method == 'GET':
         # Create a request to fake data API
         api_response = requests.get("https://reqres.in/api/users/2")
-        
+
         return Response(api_response)
