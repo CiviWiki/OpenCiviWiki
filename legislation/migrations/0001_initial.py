d7aa7d0e12f082882d30e58eac09b5d8e46eb91c
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-08-25 16:32:58 +0300

    Add initial Bill model (identifier, name, description)

---
+++ legislation/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Bill',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('identifier', models.CharField(max_length=255)),
+                ('name', models.CharField(max_length=255)),
+                ('description', models.TextField(max_length=500)),
+            ],
+        ),
+    ]
--- legislation/models.py
+++ legislation/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Bill(models.Model):
+    identifier = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=500)
