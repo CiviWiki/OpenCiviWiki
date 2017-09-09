345e2987245268fc2f5589ab10716d7cc6368d9d
Trac Taylor <trac_t@swbell.net>
2017-09-08 19:19:42 -0700

    removed commented out code on lines 99..100

--- project/api/models/civi.py
+++ project/api/models/civi.py
@@ -96,9 +96,6 @@ class Civi(models.Model):
     )
     c_type = models.CharField(max_length=31, default='problem', choices=c_CHOICES)
 
-    # sources = ArrayField(models.CharField(max_length=127, blank=True), default=[], blank=True)
-    # attachments
-
     votes_vneg = models.IntegerField(default=0)
     votes_neg = models.IntegerField(default=0)
     votes_neutral = models.IntegerField(default=0)
