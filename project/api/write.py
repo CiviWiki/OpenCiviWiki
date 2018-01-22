f2669f5a084857ef2b09893bc9dd646ab094a5bd
JooHee Lee <joohee@civiwiki.org>
2018-01-22 11:38:16 -0600

    remove unrelated change and revert to orginal file state

--- project/api/write.py
+++ project/api/write.py
@@ -254,64 +254,45 @@ def deleteCivi(request):
 
 @login_required
 def editThread(request):
+
     thread_id = request.POST.get('thread_id')
-
-    if not thread_id:
-        return HttpResponseBadRequest(reason="Invalid Thread Reference")
-
-    # Change Publish State to True
-    is_draft = request.POST.get('is_draft', True)
-
-    if not is_draft:
-        try:
-            draft_thread = Thread.objects.get(id=thread_id)
-        except Exception as e:
-            return HttpResponseServerError(reason=str(e))
-
-        draft_thread.is_draft = False
-
-        # try:
-        #     draft_thread.save()
-        # except Exception as e:
-        #     return HttpResponseServerError(reason=str(e))
-
-        return JsonResponse({'data': "Success"})
-
     title = request.POST.get('title')
     summary = request.POST.get('summary')
     category_id = request.POST.get('category_id')
     level = request.POST.get('level')
     state = request.POST.get('state')
+    if not thread_id:
+        return HttpResponseBadRequest(reason="Invalid Thread Reference")
+    else:
+        try:
+            req_edit_thread = Thread.objects.get(id=thread_id)
 
-    try:
-        req_edit_thread = Thread.objects.get(id=thread_id)
+            category_id = request.POST.get('category_id')
+            if request.user.username != req_edit_thread.author.user.username:
+                return HttpResponseBadRequest("No Edit Rights")
 
-        category_id = request.POST.get('category_id')
-        if request.user.username != req_edit_thread.author.user.username:
-            return HttpResponseBadRequest("No Edit Rights")
+            req_edit_thread.title = title
+            req_edit_thread.summary = summary
+            req_edit_thread.category_id = category_id
+            req_edit_thread.level = level
+            req_edit_thread.state = state
+            req_edit_thread.save()
+        except Exception as e:
+            return HttpResponseServerError(reason=str(e))
 
-        req_edit_thread.title = title
-        req_edit_thread.summary = summary
-        req_edit_thread.category_id = category_id
-        req_edit_thread.level = level
-        req_edit_thread.state = state
-        req_edit_thread.save()
-    except Exception as e:
-        return HttpResponseServerError(reason=str(e))
-
-    return_data = {
-        'thread_id': thread_id,
-        'title': req_edit_thread.title,
-        'summary': req_edit_thread.summary,
-        "category": {
-            "id": req_edit_thread.category.id,
-            "name": req_edit_thread.category.name
-        },
-        "level": req_edit_thread.level,
-        "state": req_edit_thread.state if req_edit_thread.level == "state" else "",
-        "location": req_edit_thread.level if not req_edit_thread.state else dict(US_STATES).get(req_edit_thread.state),
-    }
-    return JsonResponse({'data': return_data})
+        return_data = {
+            'thread_id': thread_id,
+            'title': req_edit_thread.title,
+            'summary': req_edit_thread.summary,
+            "category": {
+                "id": req_edit_thread.category.id,
+                "name": req_edit_thread.category.name
+            },
+            "level": req_edit_thread.level,
+            "state": req_edit_thread.state if req_edit_thread.level == "state" else "",
+            "location": req_edit_thread.level if not req_edit_thread.state else dict(US_STATES).get(req_edit_thread.state),
+        }
+        return JsonResponse({'data': return_data})
 
 
 @login_required
