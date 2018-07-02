32c714c91a0bd5410d8298258df826ed521ceca3
GokuMizuno <gordon.stangler@gmail.com>
2018-07-02 11:53:29 -0400

    Sunlight to ProPublica
    Upload 5/13.  Uploading individual files, since a mass delta isn't fulfilling.

--- project/civiwiki/settings.py
+++ project/civiwiki/settings.py
@@ -121,7 +121,7 @@ ADMINS = [('Development Team', 'dev@civiwiki.org')]
 
 
 # API keys
-SUNLIGHT_API_KEY = get_env_variable("SUNLIGHT_API_KEY")
+PROPUBLICA_API_KEY = get_env_variable("SUNLIGHT_API_KEY")
 GOOGLE_API_KEY = get_env_variable("GOOGLE_MAP_API_KEY")
 
 # Channels Setup
