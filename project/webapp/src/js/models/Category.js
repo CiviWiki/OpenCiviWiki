75d1ad6e0e4fece0e2041372b9c7dae0f8002fa6
JooHee Lee <joohee@civiwiki.org>
2018-07-03 01:42:35 -0500

    add category model

--- project/webapp/src/js/models/Category.js
+++ project/webapp/src/js/models/Category.js
@@ -1,6 +1,18 @@
-import { Model } from 'backbone';
-
+import { Model } from "backbone";
 
 const Category = Model.extend({
+  defaults: function() {
+    return {
+      username: "",
+      first_name: "",
+      last_name: "",
+      about_me: ""
+    };
+  },
+  urlRoot: "/api/v1/accounts/",
 
-})
+  idAttribute: "username",
+  initialize: function(model, options) {
+    options = options || {};
+  }
+});
