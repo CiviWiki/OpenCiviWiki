1026636c29ccf2b90466a8cd2c132e07fa7de64a
JooHee Lee <joohee@civiwiki.org>
2018-07-21 20:08:10 -0500

    api changes

--- project/api/models/account.py
+++ project/api/models/account.py
@@ -139,7 +139,7 @@ class Account(models.Model):
         a CITY, COUNTRY string for locations outside of the US
         """
         if self.country:
-            if self.country is "United States":
+            if self.country == "United States":
                 if self.city and self.state:
                     # Get US State from US States dictionary
                     us_state = dict(US_STATES).get(self.state)
@@ -149,7 +149,7 @@ class Account(models.Model):
                     # Get US State from US States dictionary
                     us_state = dict(US_STATES).get(self.state)
 
-                    return '{state}'.format(us_state)
+                    return '{state}'.format(state=us_state)
                 else:
                     return 'NO LOCATION'
             else:
