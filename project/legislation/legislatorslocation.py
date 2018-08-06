5957c00d8bab077deb5efdad793ae93225049091
GokuMizuno <gordon.stangler@gmail.com>
2018-08-06 07:27:43 -0400

    Returns legislators from zip.
    Removed custom legislator and district  selection code, replaced with MemberClient filters.
    Returns congresscritters, still need to return district.

--- project/legislation/legislatorslocation.py
+++ project/legislation/legislatorslocation.py
@@ -11,29 +11,33 @@ def get_legislator_and_district(account):
     rleg_list = []
 
     if ((location < 10000)||(location > 100000)):
-        throw BadRequestException #custom error this
+        #Get zip and ward from Lat/Long?
+            #yeah, we will save that for later, it is crazy difficult right now
+            #For now, throw Bad Zip error.
+        throw BadZipCodeException #custom error this
     else:  #valid zip
         #get state, and ward, then get congresscritter for ward
         #Senator is just the state, House is the hard one
-        sleg_list = MembersClient.filter("senate", #current_congress, account.state, account.district)
+        sleg_list = MembersClient.filter(senate, CURRENT_CONGRESS, account.state)
+        rleg_list = MembersClient.filter(house, CURRENT_CONGRESS, account.state, account.district)
+        #Still need to get district from location
         
-
     #Getting it from the ProPublica page works while we are small, but there is a limit
     #of 5000 requests per day, so we need to download, and store the info locally
     #update it once per day
-
+    return (sleg_list, rleg_list)
     
 
-    return NULL;
-
-def get_legislator_ids_by_lat_long(latitude, longitude):
-    """
-    Gets IDs by latitude and longitude.
-    Pulls from stored location first, if that is empty, pull from ProPublica
-    """
-    leg_list = []
-
-    try:
-        legislators =
-        except BadRequestException:
-            return leg_list
+    
+#duplicate of getLegislators(account) in account.js?
+#def get_legislator_ids_by_lat_long(latitude, longitude):
+#    """
+#    Gets IDs by latitude and longitude.
+#    Pulls from stored location first, if that is empty, pull from ProPublica
+#    """
+#    leg_list = []
+#
+#    try:
+#        legislators =
+#        except BadRequestException:
+#            return leg_list
