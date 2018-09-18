f88a2c50a46b9d9c59c9b5f8b7a5aebfa8838d4b
GokuMizuno <gordon.stangler@gmail.com>
2018-09-18 19:30:12 -0400

    Removed conflicts
    Removed conflicts between dev and Propublica branches

--- project/api/serializers.py
+++ project/api/serializers.py
@@ -1,78 +1,4 @@
 from rest_framework import serializers
-<<<<<<< HEAD
-from api.models import Civi, Thread, Account, Category
-
-
-WRITE_ONLY = {'write_only': True}
-
-class AccountSerializer(serializers.ModelSerializer):
-    username = serializers.ReadOnlyField(source='user.username')
-
-    profile_image_url = serializers.ReadOnlyField()
-    profile_image_thumb_url = serializers.ReadOnlyField()
-
-    longitude = serializers.DecimalField(max_digits=9, decimal_places=6, coerce_to_string=False)
-    latitude = serializers.DecimalField(max_digits=9, decimal_places=6, coerce_to_string=False)
-    location = serializers.ReadOnlyField()
-    civis = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-
-    class Meta:
-        model = Account
-        fields = ('id', 'username', 'first_name', 'last_name', 'about_me', 'location',
-        'address', 'city', 'state', 'zip_code', 'longitude', 'latitude',
-        'profile_image', 'profile_image_url', 'profile_image_thumb_url',
-        'civis')
-
-        # NOTE: Not sure if write only should be indicated above like read only
-        extra_kwargs = {
-            'profile_image': WRITE_ONLY,
-            'address': WRITE_ONLY,
-            'city': WRITE_ONLY,
-            'state': WRITE_ONLY,
-            'zip_code': WRITE_ONLY,
-            'longitude': WRITE_ONLY,
-            'latitude': WRITE_ONLY,
-        }
-
-    def validate_longitude(self, value):
-        """
-        Check that the blog post is about Django.
-        """
-        try:
-            long = float(value)
-        except ValueError:
-            raise serializers.ValidationError("Longitude value cannot be converted")
-
-        if int > 180 or int < -180:
-            raise serializers.ValidationError("Invalid Longitude Value")
-        return int
-
-    def validate_latitude(self, value):
-        """
-        Check that the blog post is about Django.
-        """
-        try:
-            lat = float(value)
-        except ValueError:
-            raise serializers.ValidationError("Latitude value cannot be converted")
-
-        if lat > 90 or lat < -90:
-            raise serializers.ValidationError("Invalid Latitude Value")
-        return lat
-
-
-
-class CiviSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.full_name') #TODO: more detailed
-    type = serializers.CharField(source='c_type')
-
-    class Meta:
-        model = Civi
-        fields = ('id', 'type', 'title', 'body')
-
-class CategorySerializer(serializers.ModelSerializer):
-=======
-
 from api.forms import UpdateProfileImage
 from api.models import Civi, Thread, Account, Category, CiviImage, Activity
 from utils.constants import CIVI_TYPES
@@ -220,21 +146,11 @@ class CiviListSerializer(serializers.ModelSerializer):
 
 
 class CategoryListSerializer(serializers.ModelSerializer):
->>>>>>> f5feb83f8f912194e913b0931668541112143eb6
     class Meta:
         model = Category
         fields = ('id', 'name')
 
 
-<<<<<<< HEAD
-class ThreadSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.full_name') #TODO: more detailed
-    civis = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-
-    class Meta:
-        model = Thread
-        fields = ('id', 'title', 'summary', 'author', 'category_id', 'image_url', 'civis')
-=======
 class CategorySerializer(serializers.ModelSerializer):
     preferred = serializers.SerializerMethodField()
 
@@ -329,4 +245,3 @@ class ThreadDetailSerializer(serializers.ModelSerializer):
             return [{'civi_id':activity.civi.id, 'activity_type': activity.activity_type, 'c_type': activity.civi.c_type} for activity in user_activities]
         else:
             return []
->>>>>>> f5feb83f8f912194e913b0931668541112143eb6
