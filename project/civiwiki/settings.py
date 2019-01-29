862da4ce51ab83bb5acb51bc5af692e1b4e5d615
Brylie Christopher Oxley <brylie@amble.fi>
2019-01-29 20:42:18 +0200

    Fix WhiteNoise configuration
    Signed-off-by: Brylie Christopher Oxley <brylie@amble.fi>

--- project/civiwiki/settings.py
+++ project/civiwiki/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 )
 
 
