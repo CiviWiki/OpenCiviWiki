781cca9a41e6743c29a678cc4a61b0c70a66a833
Tomasz Kołek <tomaszkolek0@gmail.com>
2019-02-26 11:02:02 +0100

    Review fixes.

--- project/api/propublica.py
+++ project/api/propublica.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 
 class ProPublicaAPI(object):
-    URL = 'https://api.propublica.org/congress/v1/bills/search.json?query="{query}"'
+    URL = 'https://api.propublica.org/congress/v1/{rest}'
 
     def __init__(self, api_key=None):
         self.api_key = api_key or settings.PROPUBLICA_API_KEY
@@ -12,6 +12,17 @@ class ProPublicaAPI(object):
     def search(self, query):
         if not self.api_key:
             return {}
-        response = requests.get(self.URL.format(query=query), headers=self.auth_headers)
+        api_rest = 'bills/search.json?query="{query}"'.format(query=query)
+        response = requests.get(self.URL.format(rest=api_rest), headers=self.auth_headers)
+        response.raise_for_status()
+        return response.json()
+
+    def get_by_id(self, bill_id):
+        if not self.api_key:
+            return {}
+
+        bill_id, congress = bill_id.split('-')
+        api_rest = "{congress}/bills/{bill_id}.json".format(congress=congress, bill_id=bill_id)
+        response = requests.get(self.URL.format(rest=api_rest), headers=self.auth_headers)
         response.raise_for_status()
         return response.json()
