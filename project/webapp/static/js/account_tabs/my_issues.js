acc75b31b9c160a4964021cfcc4112d597308477
Brylie Christopher Oxley <brylie@amble.fi>
2019-05-09 22:37:05 +0300

    Lint

--- project/webapp/static/js/account_tabs/my_issues.js
+++
@@ -1,1 +0,0 @@
-// TODO: issues (1. move from general account to here 2. consider merging with civis and creating a summary view)
--- project/webapp/static/js/static/landing_view.js
+++ project/webapp/static/js/static/landing_view.js
@@ -1,22 +1,22 @@
 cw = cw || {};
 
 cw.LandingView = BB.View.extend({
-    el: '#landing',
-    landingTemplate: _.template($('#landing-template').html()),
+  el: "#landing",
+  landingTemplate: _.template($("#landing-template").html()),
 
-    initialize: function () {
-        this.render();
-    },
+  initialize: function() {
+    this.render();
+  },
 
-    render: function () {
-        this.$el.empty().append(this.landingTemplate());
+  render: function() {
+    this.$el.empty().append(this.landingTemplate());
 
-        this.setupStaticNav();
-    },
+    this.setupStaticNav();
+  },
 
-    setupStaticNav: function () {
-        $('.button-collapse').sideNav();
-        $('.collapsible').collapsible();
-        $('.sideNav').css('display', 'inherit');
-    }
+  setupStaticNav: function() {
+    $(".button-collapse").sideNav();
+    $(".collapsible").collapsible();
+    $(".sideNav").css("display", "inherit");
+  }
 });
