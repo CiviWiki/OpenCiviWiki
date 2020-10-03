1688d043d10a103a3bbecc4931dc69894198e398
Samanway <43331341+sampoprock@users.noreply.github.com>
2020-10-03 18:52:34 +0530

    Update manage.py

--- project/manage.py
+++ project/manage.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+from django.core.management import execute_from_command_line
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "civiwiki.settings")
 
-    from django.core.management import execute_from_command_line
+    
 
     execute_from_command_line(sys.argv)
