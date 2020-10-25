6189e8e5fddc1dc21fe01237b5ae1c246437006b
Brian Painter <brianpainter@tindallcorp.com>
2020-10-24 21:54:47 -0400

    update docker files to run properly

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
-localserver()
-{
-    sleep 10
-    xvfb-run java -Dwebdriver.chrome.driver=/usr/bin/chromedriver -jar /usr/selenium-server-standalone.jar &
-    chromedriver --url-base=/wd/hub --whitelisted-ips="" &
 
-    python /app/project/manage.py migrate
-    python /app/project/manage.py runserver 0.0.0.0:8000
-}
-
-for cmd in $@
-do
-   $cmd
+echo "Waiting for postgres..."
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  sleep 0.1
 done
+echo "PostgreSQL started"
+
+
+# Collect static files
+echo "Collect static files"
+python project/manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python project/manage.py migrate
+
+# Start server
+echo "Starting server"
+python project/manage.py runserver 0.0.0.0:8000
+
+# done
+
+exec "$@"
