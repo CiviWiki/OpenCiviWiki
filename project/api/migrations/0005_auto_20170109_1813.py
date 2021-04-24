be39522665f75c8128b21405d0a9b8df49a6cc2c
harsha vardhan <hv2595@gmail.com>
2021-04-24 18:19:41 +0530

    code format with black

--- project/api/migrations/0005_auto_20170109_1813.py
+++ project/api/migrations/0005_auto_20170109_1813.py
@@ -7,23 +7,34 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0004_auto_20161230_0412'),
+        ("api", "0004_auto_20161230_0412"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='civi',
-            name='related_civis',
-            field=models.ManyToManyField(related_name='_related_civis_+', to='api.Civi'),
+            model_name="civi",
+            name="related_civis",
+            field=models.ManyToManyField(
+                related_name="_related_civis_+", to="api.Civi"
+            ),
         ),
         migrations.AlterField(
-            model_name='civi',
-            name='c_type',
-            field=models.CharField(default=b'problem', max_length=31, choices=[(b'problem', b'Problem'), (b'cause', b'Cause'), (b'solution', b'Solution'), (b'response', b'Response')]),
+            model_name="civi",
+            name="c_type",
+            field=models.CharField(
+                default=b"problem",
+                max_length=31,
+                choices=[
+                    (b"problem", b"Problem"),
+                    (b"cause", b"Cause"),
+                    (b"solution", b"Solution"),
+                    (b"response", b"Response"),
+                ],
+            ),
         ),
         migrations.AddField(
-            model_name='civi',
-            name='links',
-            field=models.ManyToManyField(related_name='link', to='api.Civi'),
+            model_name="civi",
+            name="links",
+            field=models.ManyToManyField(related_name="link", to="api.Civi"),
         ),
     ]
