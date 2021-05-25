ed8e5e799a50f94e965acd56a491d2d084e98017
Brylie Christopher Oxley <brylie@amble.fi>
2021-05-25 16:17:17 +0300

    Fix syntax error

--- project/civiwiki/settings.py
+++ project/civiwiki/settings.py
@@ -144,9 +144,9 @@ if "CIVIWIKI_LOCAL_NAME" not in os.environ:
 else:
     if DEBUG:
         DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME": BASE_DIR + "/" + "db.sqlite3",
             }
         }
     else:
