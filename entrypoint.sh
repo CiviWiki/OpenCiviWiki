7f090169c7d85c800bdb9555934d1bbe4bee9f66
Brylie Christopher Oxley <brylie@amble.fi>
2021-05-26 06:38:26 +0300

    Remove chromedriver commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,6 @@
 
 localserver()
 {
-    sleep 10
-    xvfb-run java -Dwebdriver.chrome.driver=/usr/bin/chromedriver -jar /usr/selenium-server-standalone.jar &
-    chromedriver --url-base=/wd/hub --whitelisted-ips="" &
-
     python /app/project/manage.py migrate
     python /app/project/manage.py runserver 0.0.0.0:8000
 }
