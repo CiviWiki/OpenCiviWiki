4726893c3aa5ca93b783b4f96c1f1d75a37f2829
Brylie Christopher Oxley <brylie@amble.fi>
2021-05-27 12:56:02 +0300

    Remove imports from deleted models

--- project/api/models/__init__.py
+++ project/api/models/__init__.py
@@ -3,7 +3,6 @@ Gathering all models here
 """
 
 from .account import Account
-from .bill import Bill
 from .category import Category
 from .civi import Civi, CiviImage
 from .fact import Fact
@@ -12,6 +11,5 @@ from .rationale import Rationale
 from .rebuttal import Rebuttal
 from .response import Response
 from .thread import Thread
-from .vote import Vote
 from .activity import Activity
 from .invitation import Invitation
