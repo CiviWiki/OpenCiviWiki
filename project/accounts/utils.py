c57abe524b4a4f322ef1601076a5f73b821ce45d
Ahmed Nassar <ektebli@yahoo.com>
2021-06-19 01:07:00 +0200

    Removed Celery and all related code from the app.

--- project/accounts/utils.py
+++ project/accounts/utils.py
@@ -1,3 +1,5 @@
+from django.core.mail import get_connection, EmailMultiAlternatives
+from django.template.loader import render_to_string
 from django.conf import settings
 from django.core.mail import send_mail
 
@@ -16,3 +18,28 @@ def send_email(subject, message, sender, recipient_list, html_message=None):
         recipient_list,  # Target Email
         html_message=html_message,  # text/html Content
     )
+
+
+def send_mass_email(subject, contexts):
+    """ Construct and send a multipart/alternative email """
+
+    # Manually open connection to the SMTP server specified in settings.py
+    connection = get_connection()  # uses SMTP server specified in settings.py
+    connection.open()
+
+    messages = []
+    for email_context in contexts:
+        msg_plain = render_to_string("email/base_text_template.txt", email_context)
+        msg_html = render_to_string("email/base_email_template.html", email_context)
+
+        msg = EmailMultiAlternatives(
+            subject,  # Subject Line
+            msg_plain,  # text/plain Content
+            settings.EMAIL_HOST_USER,  # Sender Email (default: support@civiwiki.org)
+            email_context["recipient"],  # Target Email
+            connection=connection,
+        )
+        msg.attach_alternative(msg_html, "text/html")  # text/html Content
+        messages.append(msg)
+
+    connection.send_messages(messages)
