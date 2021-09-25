05dc07f40b7e915d8c9cd04de17f04c247491f34
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-09-25 21:16:08 +0530

    Formatting and other migrations to models

--- project/threads/models.py
+++ project/threads/models.py
@@ -14,6 +14,18 @@ from taggit.managers import TaggableManager
 from django.core.serializers.json import DjangoJSONEncoder
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=63)
+
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    last_modified = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
+
 class Fact(models.Model):
     body = models.CharField(max_length=511)
 
