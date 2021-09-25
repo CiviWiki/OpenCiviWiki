b5baa404f27178a7c7dad368edbcf7dc00c83b5a
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-09-25 21:21:22 +0530

    Changes to accomodate viewset

--- project/threads/urls.py
+++ project/threads/urls.py
@@ -1,30 +1,31 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
-from api import views, read, write
+from .api import *
+from .serializers import ThreadViewSet,CiviViewSet
 
 router = DefaultRouter(trailing_slash=False)
-router.register(r"threads", views.ThreadViewSet)
-router.register(r"civis", views.CiviViewSet)
+router.register(r"threads", ThreadViewSet)
+router.register(r"civis", CiviViewSet)
 
 urlpatterns = [
     url(r"^v1/", include(router.urls)),
 ]
 
 urlpatterns += [
-    url(r"^thread_data/(?P<thread_id>\w+)/$", read.get_thread, name="get thread"),
-    url(r"^civi_data/(?P<civi_id>\w+)$", read.get_civi, name="get civi"),
-    url(r"^threads/(?P<thread_id>\w+)/civis$", read.get_civis, name="get civis"),
+    url(r"^thread_data/(?P<thread_id>\w+)/$", get_thread, name="get thread"),
+    url(r"^civi_data/(?P<civi_id>\w+)$", get_civi, name="get civi"),
+    url(r"^threads/(?P<thread_id>\w+)/civis$", get_civis, name="get civis"),
     url(
         r"^response_data/(?P<thread_id>\w+)/(?P<civi_id>\w+)/$",
-        read.get_responses,
+        get_responses,
         name="get responses",
     ),
-    url(r"^feed/$", read.get_feed, name="get thread"),
-    url(r"^new_thread/$", write.new_thread, name="new thread"),
-    url(r"^edit_thread/$", write.editThread, name="edit thread"),
-    url(r"^new_civi/$", write.createCivi, name="new civi"),
-    url(r"^rate_civi/$", write.rateCivi, name="rate civi"),
-    url(r"^edit_civi/$", write.editCivi, name="edit civi"),
-    url(r"^delete_civi/$", write.deleteCivi, name="delete civi"),
+    url(r"^feed/$", get_feed, name="get thread"),
+    url(r"^new_thread/$", new_thread, name="new thread"),
+    url(r"^edit_thread/$", edit_thread, name="edit thread"),
+    url(r"^new_civi/$", create_civi, name="new civi"),
+    url(r"^rate_civi/$", rate_civi, name="rate civi"),
+    url(r"^edit_civi/$", edit_civi, name="edit civi"),
+    url(r"^delete_civi/$", delete_civi, name="delete civi"),
 ]
