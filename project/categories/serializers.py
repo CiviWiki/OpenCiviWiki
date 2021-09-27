7332fbbfac90fdc6b8f2363d9e6785c3953ff86c
Brylie Christopher Oxley <brylie@amble.fi>
2021-09-27 12:17:54 +0300

    Update project/categories/serializers.py

--- project/categories/serializers.py
+++ project/categories/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-
+from .models import Category
+from accounts.models import Profile
 
 class CategoryListSerializer(serializers.ModelSerializer):
     """ """
