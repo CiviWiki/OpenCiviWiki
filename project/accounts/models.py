c272440037ecc02bc6be65693a56d4e354b90869
ANURAG VERMA <20cse08@ietagra.org.in>
2021-09-27 07:37:27 +0530

    removed file views.py and test.py from categories app

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -9,7 +9,7 @@ from django.core.files.uploadedfile import InMemoryUploadedFile
 
 from taggit.managers import TaggableManager
 
-from api.models.category import Category
+from categories.models import Category
 from common.utils import PathAndRename
 
 
