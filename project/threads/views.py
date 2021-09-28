b0e0991eb5429c351f51544047fc6d49286138ae
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-09-28 16:56:33 +0530

    Added multiple viewsets to accommodate api deprecation

--- project/threads/views.py
+++ project/threads/views.py
@@ -1,3 +1,127 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from .models import Thread
+from .serializers import ThreadSerializer, CategorySerializer
+from categories.models import Category
+from accounts.api import ProfilewViewSet
+
+
+from .models import Thread, Civi, CiviImage
+from .serializers import (
+    ThreadSerializer,
+    ThreadListSerializer,
+    ThreadDetailSerializer,
+    CiviSerializer,
+    CiviImageSerializer
+)
+
+from .utils import get_account
+from .permissions import IsOwnerOrReadOnly
+
+class ThreadViewSet(ModelViewSet):
+    queryset = Thread.objects.order_by("-created")
+    serializer_class = ThreadDetailSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
+
+    def list(self, request):
+        threads = Thread.objects.filter(is_draft=False).order_by("-created")
+        serializer = ThreadSerializer(threads, many=True, context={"request": request})
+        return Response(serializer.data)
+
+    def get_queryset(self):
+        queryset = Thread.objects.all()
+        category_id = self.request.query_params.get("category_id", None)
+        if category_id is not None:
+            if category_id != "all":
+                queryset = queryset.filter(category=category_id)
+
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(author=get_account(user=self.request.user))
+
+    @action(detail=True)
+    def civis(self, request, pk=None):
+        """
+        Gets the civis linked to the thread instance
+        /threads/{id}/civis
+        """
+        thread_civis = Civi.objects.filter(thread=pk)
+        serializer = CiviSerializer(thread_civis, many=True)
+        return Response(serializer.data)
+
+    @action(methods=["get", "post"], detail=False)
+    def all(self, request):
+        """
+        Gets the all threads for listing
+        /threads/all
+        """
+        all_threads = self.queryset
+        serializer = ThreadListSerializer(
+            all_threads, many=True, context={"request": request}
+        )
+        return Response(serializer.data)
+
+    @action(methods=["get", "post"], detail=False)
+    def top(self, request):
+        """
+        Gets the top threads based on the number of page views
+        /threads/top
+        """
+        limit = request.query_params.get("limit", 5)
+        top_threads = Thread.objects.filter(is_draft=False).order_by("-num_views")[
+            :limit
+        ]
+        serializer = ThreadListSerializer(
+            top_threads, many=True, context={"request": request}
+        )
+        return Response(serializer.data)
+
+    @action(detail=False)
+    def drafts(self, request):
+        """
+        Gets the drafts of the current authenticated user
+        /threads/drafts
+        """
+        account = get_account(username=self.request.user)
+        draft_threads = Thread.objects.filter(author=account, is_draft=True)
+        serializer = ThreadListSerializer(
+            draft_threads, many=True, context={"request": request}
+        )
+        return Response(serializer.data)
+
+class CiviViewSet(ModelViewSet):
+    """ REST API viewset for Civis """
+
+    queryset = Civi.objects.all()
+    serializer_class = CiviSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(author=get_account(user=self.request.user))
+
+    @action(detail=True)
+    def images(self, request, pk=None):
+        """
+        Gets the related images
+        /civis/{id}/images
+        """
+        civi_images = CiviImage.objects.filter(civi=pk)
+        serializer = CiviImageSerializer(civi_images, many=True, read_only=True)
+        return Response(serializer.data)
+
+class CategoryViewSet(ReadOnlyModelViewSet):
+    """ REST API viewset for Categories """
+
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    authentication_classes = ()
+
+    @action(detail=True)
+    def threads(self, request, pk=None):
+        category_threads = Thread.objects.filter_by_category_id(pk)
+        serializer = ThreadSerializer(category_threads, many=True)
+        return Response(serializer.data)
