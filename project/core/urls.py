614280f4e2407fdc255d3b1269d6b93dbed70d24
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-10-01 23:23:06 +0530

    Migrated urls to path according to Django 3.2

--- project/core/urls.py
+++ project/core/urls.py
@@ -14,51 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.conf.urls import url
-from django.urls import path, include
 from django.contrib import admin
-from django.conf import settings
-from django.views.static import serve
+from django.urls import include, path
 from django.views.generic.base import RedirectView
-from core.router import CiviWikiRouter
-from threads import urls as api
 from frontend_views import urls as frontend_views
+from threads import urls as threads
 
+from core.router import CiviWikiRouter
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('accounts.urls.urls')),
     path("api/", include('accounts.urls.api')),
     path('api/v1/', include((CiviWikiRouter.urls, 'api'))),
-    url(r"^api/", include(api)),
-    url(
-        "^inbox/notifications/",
-        include("notifications.urls", namespace="notifications"),
-    ),
+    path("api/", include(threads)),
+    path("inbox/notifications/",include("notifications.urls", namespace="notifications")),
+    path("favicon.ico", RedirectView.as_view(url="/static/favicon/favicon.ico")),
+    path("favicon-32x32.png",RedirectView.as_view(url="/static/favicon/favicon-32x32.png")),
+    path("apple-touch-icon.png", RedirectView.as_view(url="/static/favicon/apple-touch-icon.png")),
+    path("mstile-144x144.png", RedirectView.as_view(url="/static/favicon/mstile-144x144.png")),
+    path("", include(frontend_views))
 ]
 
-urlpatterns += [
-    # A redirect for favicons at the root of the site
-    url(r"^favicon\.ico$", RedirectView.as_view(url="/static/favicon/favicon.ico")),
-    url(
-        r"^favicon-32x32\.png$",
-        RedirectView.as_view(url="/static/favicon/favicon-32x32.png"),
-    ),
-    url(
-        r"^apple-touch-icon\.png$",
-        RedirectView.as_view(url="/static/favicon/apple-touch-icon.png"),
-    ),
-    url(
-        r"^mstile-144x144\.png$",
-        RedirectView.as_view(url="/static/favicon/mstile-144x144.png"),
-    ),
-    # Media and Static file Serve Setup.
-    url(
-        r"^media/(?P<path>.*)$",
-        serve,
-        {"document_root": settings.MEDIA_ROOT, "show_indexes": True},
-    ),
-    url(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
-    url(r"^", include(frontend_views)),
-
-]
