a69d6e94d6a1a8bea46bdf91b525bbf2e061b73e
grkm <garslan13@gmail.com>
2021-10-02 22:51:42 +0300

    Update frontend urls

--- project/frontend_views/urls.py
+++ project/frontend_views/urls.py
@@ -1,14 +1,13 @@
-from django.conf.urls import url
-from . import views as v
+from django.urls import path
+from frontend_views import views
 
 urlpatterns = [
-    url(r"^about$", v.about_view, name="about"),
-    url(r"^support_us$", v.support_us_view, name="support us"),
-    url(r"^howitworks$", v.how_it_works_view, name="how it works"),
-    url(r"^profile/(?P<username>[a-zA-Z0-9-_]*)$", v.user_profile, name="profile"),
-    url(r"^profile/rep/(?P<username>\d+)$", v.user_profile, name="profile"),
-    url(r"^thread/(?P<thread_id>\w+)$", v.issue_thread, name="issue thread"),
-    url(r"^profile$", v.user_profile, name="default_profile"),
-    url(r"^$", v.base_view, name="base"),
-    url(r"^thread/(?P<thread_id>\w+)/csv$", v.civi2csv, name="civi2csv"),
+    path("about/", views.about_view, name="about"),
+    path("support_us/", views.support_us_view, name="support us"),
+    path("howitworks/", views.how_it_works_view, name="how it works"),
+    path("profile/<str:username>/", views.user_profile, name="profile"),
+    path("thread/<int:thread_id>/", views.issue_thread, name="issue thread"),
+    path("profile/", views.user_profile, name="default_profile"),
+    path("", views.base_view, name="base"),
+    path("thread/<int:thread_id>)/csv/", views.civi2csv, name="civi2csv"),
 ]
