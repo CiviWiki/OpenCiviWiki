b0ed96a6e5ba072f031ca935b8b50cd127f2c04a
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-10-07 21:19:39 +0530

    Removed profile related urls

--- project/frontend_views/urls.py
+++ project/frontend_views/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
-from frontend_views import views
+from frontend_views.views import (
+    about_view,
+    support_us_view,
+    how_it_works_view,
+    issue_thread,
+    civi2csv,
+    base_view,
+)
 
 urlpatterns = [
-    path("about/", views.about_view, name="about"),
-    path("support_us/", views.support_us_view, name="support us"),
-    path("howitworks/", views.how_it_works_view, name="how it works"),
-    path("profile/<str:username>/", views.user_profile, name="profile"),
-    path("thread/<int:thread_id>/", views.issue_thread, name="issue thread"),
-    path("profile/", views.user_profile, name="default_profile"),
-    path("", views.base_view, name="base"),
-    path("thread/<int:thread_id>/csv/", views.civi2csv, name="civi2csv"),
+    path("about/", about_view, name="about"),
+    path("support_us/", support_us_view, name="support us"),
+    path("howitworks/", how_it_works_view, name="how it works"),
+    path("thread/<int:thread_id>/", issue_thread, name="issue thread"),
+    path("", base_view, name="base"),
+    path("thread/<int:thread_id>/csv/", civi2csv, name="civi2csv"),
 ]
