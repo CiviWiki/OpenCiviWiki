fe4f98e93ef02c6dee9d5b5109ce7e96ff7643b9
grkm <garslan13@gmail.com>
2021-10-12 19:51:47 +0300

    Apply changes in PR#1116

--- project/threads/urls/urls.py
+++ project/threads/urls/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path("thread/<int:thread_id>/csv/", views.civi2csv, name="civi2csv"),
     path("thread/<int:thread_id>/", views.issue_thread, name="issue_thread"),
     path("about/", views.about_view, name="about"),
-    path("support_us/", views.support_us_view, name="support us"),
-    path("howitworks/", views.how_it_works_view, name="how it works"),
+    path("support_us/", views.support_us_view, name="support_us"),
+    path("howitworks/", views.how_it_works_view, name="how_it_works"),
     path("declaration/", views.declaration, name="declaration"),
-    path("create-group/", views.create_group, name="create group"),
+    path("create-group/", views.create_group, name="create_group"),
     path("", views.base_view, name="base"),
 ]
