d67e85ca61e6a78c289911dd2d536db6a8c37a8d
grkm <garslan13@gmail.com>
2021-10-25 20:08:48 +0300

    Remove redundant url routes

--- project/threads/urls/api.py
+++ project/threads/urls/api.py
@@ -1,23 +1,14 @@
-from django.conf.urls import include
 from django.urls import path
-from rest_framework.routers import DefaultRouter
+from core.router import CiviWikiRouter
 
 from threads import api
 from threads.views import ThreadViewSet, CiviViewSet
-from categories.api import CategoryViewSet
-from accounts.api import ProfileViewSet
 
-router = DefaultRouter(trailing_slash=False)
-router.register(r"threads", ThreadViewSet)
-router.register(r"categories", CategoryViewSet)
-router.register(r"civis", CiviViewSet)
-router.register(r"accounts", ProfileViewSet)
+
+CiviWikiRouter.register(r"threads", ThreadViewSet)
+CiviWikiRouter.register(r"civis", CiviViewSet)
 
 urlpatterns = [
-    path("v1/", include(router.urls)),
-]
-
-urlpatterns += [
     path("thread_data/<int:thread_id>/", api.get_thread, name="get thread"),
     path("civi_data/<int:civi_id>/", api.get_civi, name="get civi"),
     path("threads/<int:thread_id>/civis", api.get_civis, name="get civis"),
