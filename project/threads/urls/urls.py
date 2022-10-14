35ee1d86eef5922e5edd2c533811cfb277e83146
gorkemarslan <garslan13@gmail.com>
2022-10-14 19:14:36 +0300

    Reformat URL names

--- project/threads/urls/urls.py
+++ project/threads/urls/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path("thread/<int:thread_id>/csv/", views.civi2csv, name="civi2csv"),
     path("thread/<int:pk>/", views.ThreadDetailView.as_view(), name="thread-detail"),
     path("about/", views.AboutView.as_view(), name="about"),
-    path("support_us/", views.SupportUsView.as_view(), name="support_us"),
-    path("howitworks/", views.HowItWorksView.as_view(), name="how_it_works"),
+    path("support_us/", views.SupportUsView.as_view(), name="support-us"),
+    path("howitworks/", views.HowItWorksView.as_view(), name="how-it-works"),
     path("declaration/", views.DeclarationView.as_view(), name="declaration"),
-    path("create-group/", views.create_group, name="create_group"),
+    path("create-group/", views.create_group, name="create-group"),
     path("", views.base_view, name="base"),
 ]
