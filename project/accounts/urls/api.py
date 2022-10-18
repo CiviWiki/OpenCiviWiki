5f061059a4ac5096f522bbf55c83b07d3a1a47c6
Brylie Christopher Oxley <brylie@amble.fi>
2022-10-18 22:21:57 +0300

    Remove remaining API views/URLs

--- project/accounts/urls/api.py
+++
@@ -1,9 +0,0 @@
-from accounts import api
-from django.urls import path
-
-urlpatterns = [
-    # TODO: port to Django view
-    path("follow/", api.request_follow, name="follow_user"),
-    # TODO: port to Django view
-    path("unfollow/", api.request_unfollow, name="unfollow_user"),
-]
