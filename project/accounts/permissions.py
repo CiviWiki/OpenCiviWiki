67ce9d0ccaf138241a384e5fef79c786e2b3aaf4
Brylie Christopher Oxley <brylie@amble.fi>
2022-10-20 09:28:04 +0300

    Remove unused file

--- project/accounts/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import SAFE_METHODS, BasePermission
-
-
-class IsProfileOwnerOrReadOnly(BasePermission):
-    """Custom API permission to check if request user is the owner of the profile"""
-
-    def has_object_permission(self, request, view, obj):
-        return (request.method in SAFE_METHODS) or (obj.user == request.user)
